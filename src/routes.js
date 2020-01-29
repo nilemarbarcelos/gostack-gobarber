a59b5cf453da4ed07710ec4bd3eb13ee64be6c74
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-01-29 23:45:09 +0000

    :wrench: model loader

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Nilemar Barcelos',
+    email: 'nilemarbarcelos@gmail.com',
+    password_hash: '2123123',
+  });
+
+  res.json(user);
+});
 
 export default routes;
