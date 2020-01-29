6aa3db1cc797cbf836cec0f71528b734d6408c3f
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-01-29 23:53:36 +0000

    :wrench: add user

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Nilemar Barcelos',
-    email: 'nilemarbarcelos@gmail.com',
-    password_hash: '2123123',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
