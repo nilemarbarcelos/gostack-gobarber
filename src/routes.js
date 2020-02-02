42de0a123fb14a5eb8bd4d83cc934cc027674fab
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-02-02 22:07:07 +0000

    :wrench: update notifications

--- src/routes.js
+++ src/routes.js
@@ -28,6 +28,7 @@ routes.get('/appointments', AppointmentController.index);
 routes.get('/schedules', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
