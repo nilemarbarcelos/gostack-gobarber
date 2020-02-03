968cafe65f880b24ccccac9220e1baa0d49d0144
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-02-03 20:12:25 +0000

    :wrench: use nodemailer to send cancelation email

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'd4c7f443994118',
+    pass: '46e2ad5ab49b95',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};
