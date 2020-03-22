8ec6c4c48fb48ec761c7b406d78619a6cf19e7be
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-03-22 21:46:25 +0000

    :wrench: auth styles

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Your email" />
+        <input type="password" placeholder="Your password" />
+        <button type="submit">Enter</button>
+        <Link to="/register">Create a free account</Link>
+      </form>
+    </>
+  );
 }
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="text" placeholder="Full name" />
+        <input type="email" placeholder="Your email" />
+        <input type="password" placeholder="Your password" />
+        <button type="submit">Create Account</button>
+        <Link to="/">I have an account</Link>
+      </form>
+    </>
+  );
 }
