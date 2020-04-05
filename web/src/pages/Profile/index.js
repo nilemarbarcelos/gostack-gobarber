0a6801899bbb97ad023bd8371d680a02f42144ca
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-04-05 18:34:50 +0100

    :wrench: profile page

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your e-mail address" />
+        <hr />
+        <Input
+          type="password"
+          name="oldPassword"
+          placeholder="Your current password"
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Your new password"
+        />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirm your new password"
+        />
+        <button type="submit">Update profile</button>
+      </Form>
+      <button type="button">Logout</button>
+    </Container>
+  );
 }
