8ac78a2b88ba83f4fde5963fbecd9ddbb4de6299
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-04-05 18:52:55 +0100

    :wrench: updating profile

--- web/src/store/modules/user/sagas.js
+++ web/src/store/modules/user/sagas.js
@@ -1,3 +1,22 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
-export default all([]);
+import api from '~/services/api';
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+  try {
+    const { name, email, ...rest } = payload.data;
+    const profile = { name, email, ...(rest.oldPassword ? rest : {}) };
+    const response = yield call(api.put, 'users', profile);
+
+    toast.success('Profile has been updated');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    toast.success('Error while updating profile');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
