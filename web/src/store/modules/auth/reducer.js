251897d3a3f5fd65876a0ff3811b1f5fcb858ebc
Nilemar de Barcelos <nilemarbarcelos@gmail.com>
2020-04-05 19:18:18 +0100

    :wrench: logout

--- web/src/store/modules/auth/reducer.js
+++ web/src/store/modules/auth/reducer.js
@@ -2,6 +2,7 @@ import produce from 'immer';
 
 const INITIAL_STATE = {
   token: null,
+
   signed: false,
   loading: false,
 };
@@ -29,6 +30,11 @@ export default function auth(state = INITIAL_STATE, action) {
       }
       case '@auth/SIGN_FAILURE': {
         draft.loading = false;
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.token = null;
+        draft.signed = false;
         break;
       }
       default:
